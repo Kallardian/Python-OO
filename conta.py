4f28d6b5a8a62ab17725604181b9497c441e69e0
Kallardian <paulobertanha03@gmail.com>
2021-01-27 16:58:47 -0300

    Learned about Self

--- conta.py
+++ conta.py
@@ -1,2 +1,7 @@
 class Conta:
-    pass
+    def __init__(self, numero, titular, saldo, limite): 
+        #self is a reference that finds the object in the memory, it is given automatically
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
